9255a8e76a643216c70d1245120094a4806efefb
MArslan47 <marslanch7@gmail.com>
2024-03-08 09:49:47 +0500

    add

---
+++ lib/Screens/cameraScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class Camera_Screen extends StatefulWidget {
+  const Camera_Screen({super.key});
+
+  @override
+  State<Camera_Screen> createState() => _Camera_ScreenState();
+}
+
+class _Camera_ScreenState extends State<Camera_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+
+        ),
+      ),
+    );
+  }
+}
